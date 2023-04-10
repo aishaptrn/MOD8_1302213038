181b7b7e0fdee2a9a22e8d8dd49b088412906e20
Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>
2023-04-10 08:09:40 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_1302213038/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using modul8_1302213038;
+using System.Security.Cryptography.X509Certificates;
+
+BankTransferConfig tfConfig = new BankTransferConfig();
+readJson read = new readJson();
+
+// read.bankTfCon.changeLang_1302213038();
+
+// part i & ii
+Console.WriteLine("Please insert the amount of money to transfer:");
+int mon = int.Parse(Console.ReadLine());
+
+int sum = 0;
+if (mon <= read.bankTfCon.threshold)
+{
+    sum = mon + read.bankTfCon.low_fee;
+    Console.WriteLine("Transfer fee = Rp " + read.bankTfCon.low_fee);
+    Console.WriteLine("Total amount = Rp " + sum);
+} else
+{
+    sum = mon + read.bankTfCon.high_fee;
+    Console.WriteLine("Transfer fee = Rp " + read.bankTfCon.high_fee);
+    Console.WriteLine("Total amount = Rp " + sum);
+}
+
+// part iii & iv
+Console.WriteLine("\nSelect transfer method: ");
+Console.WriteLine(read.bankTfCon.meth1);
+Console.WriteLine(read.bankTfCon.meth2);
+Console.WriteLine(read.bankTfCon.meth3);
+Console.WriteLine(read.bankTfCon.meth4);
+string meth = Console.ReadLine();
+
+// part v - vii
+Console.WriteLine("\nPlease type " + read.bankTfCon.en + " to confirm the transaction");
+string confirm = Console.ReadLine();
+
+if (confirm == read.bankTfCon.en)
+{
+    Console.WriteLine("The transfer is completed");
+} else
+{
+    Console.WriteLine("Transfer is cancelled");
+}
